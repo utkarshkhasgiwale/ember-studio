18d8a1d0069e873141427634b41da30863737533
utkarsh khasgiwale <utkarshkhasgiwale16@gmail.com>
2026-06-27 10:07:57 +0530

    Update video source path in Video component

--- ember-studio/src/components/home/Video.jsx
+++ ember-studio/src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full' style={{ boxShadow: 'inset 0 0 0 2px var(--signal)' }}>
-        <video className='h-full w-full object-cover' autoPlay loop muted playsInline src="../../../public/video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted playsInline src="/video.mp4"></video>
     </div>
   )
 }
